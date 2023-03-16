551f630300370fb7cda0f34f8ffede2c7eed1a60
Sadat15 <sadatmanjur15@gmail.com>
2023-03-16 19:01:49 +0000

    Project setup

---
+++ src/App.js
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <div>
+      <div>App</div>
+      <img
+        id="inputimage"
+        src={
+          "https://www150.statcan.gc.ca/edu/power-pouvoir/c-g/c-g05-7-1-eng.png"
+        }
+        alt=""
+        width="500px"
+        height="auto"
+      />
+      <img
+        id="inputimage"
+        src={
+          "https://www150.statcan.gc.ca/edu/power-pouvoir/c-g/c-g05-7-1-eng.png"
+        }
+        alt=""
+        width="500px"
+        height="auto"
+      />
+    </div>
+  );
+}
+
+export default App;
